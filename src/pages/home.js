cd7ad6080e8b8b191f26405b4149a05a7cc8eca8
Jomari Iringan <jomari.iringan@olegroups.com>
2021-07-21 11:55:21 +0800

    update files

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -6,31 +6,111 @@ import {
   StyleSheet,
   Text,
   useColorScheme,
+  useWindowDimensions,
   View,
 } from 'react-native';
 
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+import Geolocation from '@react-native-community/geolocation';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import mapstyle from '../mapstyle';
+import { regionFrom } from '../helper';
+
+const LATITUDE_DELTA = 0.0922
+const LONGITUDE_DELTA = 0.05
+
+const GEOLOCATION_OPTION = { 
+    enableHighAccuracy: true, 
+    timeout: 15000, 
+    maximumAge: 10000 
+}
+
+const INITIAL_STATE = {
+    region: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+    },
+    markers: []
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+      case 'UPDATE_REGION':
+        return {...INITIAL_STATE, 
+            region: action.region
+        };
+        break;
+    case 'UPDATE_MARKERS': 
+        return {...INITIAL_STATE, 
+            markers: [...state.markers, ...action.markers]};
+        break;
+      default:
+        return {...INITIAL_STATE};
+    }
+}
+
+function getMarker(title, coordinate, description = ''){
+    let generated_key = `${Math.ceil(coordinate.longitude)}${Math.floor(coordinate.latitude)}`
+    return (<Marker
+        key={generated_key}
+        coordinate={coordinate}
+        title={title}
+        description={description}
+      />)
+} 
 
 export default function Home(props){
+    const window = useWindowDimensions()
+    const {height,width} = window
+    const [state, dispatch] = useReducer(reducer, INITIAL_STATE)
+
+    useEffect(()=>{
+        const setup = () => {
+            Geolocation.getCurrentPosition(info => {
+                let { coords } = info
+                if(state.region.latitude !== coords.latitude && state.region.longitude !== coords.longitude){
+                    let createdRegion = regionFrom([coords]);
+                    dispatch({type: 'UPDATE_REGION', region: createdRegion})
+                    
+                    // dispatch({type: 'UPDATE_MARKERS', 
+                    //     markers: [getMarker('Current Position', {
+                    //         longitude: coords.longitude,
+                    //         latitude: coords.latitude
+                    //     }, 'this is a description')]
+                    // });
+                }
+                
+            },((err)=> { console.log('error')}), GEOLOCATION_OPTION)
+        }
+        setup()
+    })
+
+    const onRegionChange = (region) => {
+
+    }
+    // useEffect(()=>{
+    //     Geolocation.getCurrentPosition(info => console.log(info));
+    // })
   
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={selfStyle.container}>
-        <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={selfStyle.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-        customMapStyle={mapstyle}
-      ></MapView>
-      </View>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={{flex: 1}}>
+        <View style={selfStyle.container}>
+            <MapView
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={selfStyle.map}
+            region={state.region}
+            zoomEnabled={true}
+            minZoomLevel={0}
+            maxZoomLevel={18}
+            customMapStyle={mapstyle}     
+            onRegionChange={onRegionChange}       
+        >
+        </MapView>
+        </View>
+        </SafeAreaView>
+    );
 };
 
 const selfStyle = StyleSheet.create({
